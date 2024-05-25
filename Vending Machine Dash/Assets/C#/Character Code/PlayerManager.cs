a98f73314d1ef1cfe92da312e0d112d789fa30ee
James Zilberman <jaz1324576@gmail.com>
2024-05-25 16:54:20 +0900

    All players teleport

--- Vending Machine Dash/Assets/C#/Character Code/PlayerManager.cs
+++ Vending Machine Dash/Assets/C#/Character Code/PlayerManager.cs
@@ -15,48 +15,20 @@ public class PlayerManager : MonoBehaviour
 
     [SerializeField] private Camera cam;
 
-    private void OnEnable()
-    {
-        playerInputManager = GetComponent<PlayerInputManager>();
-        playerList = new GameObject[] { player1, player2, player3, player4 };
-
-    }
+    public int numberOfPlayers = 4;
 
     private void Awake()
     {
-        Vector3[] playerPositions = { new Vector3(5, 0, 5), new Vector3(5, 0, -5), new Vector3(-5, 0, 5), new Vector3(-5, 0, -5) };
         playerInputManager = GetComponent<PlayerInputManager>();
         playerList = new GameObject[] { player1, player2, player3, player4 };
-        for (int i = 0; i < 4; i++)
+        Vector3[] playerPositions = { new Vector3(5, 0, 5), new Vector3(5, 0, -5), new Vector3(-5, 0, 5), new Vector3(-5, 0, -5) };
+        playerInputManager = GetComponent<PlayerInputManager>();
+
+        for (int i = 0; i < numberOfPlayers; i++)
         {
             OnPlayerJoined();
             playerList[i] = playerInputManager.JoinPlayer().transform.gameObject;
             playerList[i].transform.position = playerPositions[i];
-        }
-    }
-
-    private void Update()
-    {
-        for (int i = 0; i < 4; i++) {
-            Vector3 playerPos = cam.WorldToScreenPoint(playerList[i].transform.position);
-            if (playerPos.x <= 0)
-            {
-                this.transform.position = new Vector3(this.transform.position.x - 15.925f, this.transform.position.y, this.transform.position.z);
-            }
-            else if (playerPos.x >= Screen.width)
-            {
-                this.transform.position = new Vector3(this.transform.position.x + 15.925f, this.transform.position.y, this.transform.position.z);
-            }
-            if (playerPos.y <= 0)
-            {
-                Debug.Log(playerPos);
-                this.transform.position = new Vector3(this.transform.position.x, this.transform.position.y - 8.96f, this.transform.position.z);
-            }
-            else if (playerPos.y >= Screen.height)
-            {
-                Debug.Log(playerPos);
-                this.transform.position = new Vector3(this.transform.position.x, this.transform.position.y + 8.96f, this.transform.position.z);
-            }
         }
     }
 
---
+++ Vending Machine Dash/Assets/C#/Character Code/Teleport.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleport : MonoBehaviour
+{
+    [SerializeField] private bool isHorizontal;
+    private void OnTriggerEnter(Collider other)
+    {
+        var otherstuff = other.gameObject.GetComponent<PlayerMovement>();
+        if(other.tag == "Player")
+        {
+            if (isHorizontal)
+            {
+                other.transform.position = new Vector3(-other.transform.position.x, other.transform.position.y, other.transform.position.z);
+            }
+            else
+            {
+                other.transform.position = new Vector3(other.transform.position.x, other.transform.position.y, -other.transform.position.z);
+            }
+        }
+    }
+}
